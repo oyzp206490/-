324e1ec5ea17208d415406e6df6f3f67fc184c0f
oyzp206490 <292412692@qq.com>
2018-10-23 17:53:16 +0800

    video

---
+++ video/src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<template>
+  <video-player class="video-player-box" ref="videoPlayer" :options="playerOptions" :playsinline="true" customEventName="customstatechangedeventname">
+  </video-player>
+</template>
+
+<script>
+// Similarly, you can also introduce the plugin resource pack you want to use within the component
+// import 'some-videojs-plugin'
+import 'video.js/dist/video-js.css'
+import { videoPlayer } from 'vue-video-player'
+export default {
+  components: { videoPlayer },
+  data () {
+    return {
+      playerOptions: {
+        // videojs options
+        muted: true,
+        language: 'en',
+        playbackRates: [0.7, 1.0, 1.5, 2.0],
+        sources: [{
+          type: 'video/mp4',
+          src: 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm'
+        }],
+        poster: '/static/images/author.jpg'
+      }
+    }
+  },
+  mounted () {
+    console.log('this is current player instance object', this.player)
+  },
+  computed: {
+    player () {
+      return this.$refs.videoPlayer.player
+    }
+  },
+  methods: {
+    // listen event
+    onPlayerPlay (player) {
+      // console.log('player play!', player)
+    },
+    onPlayerPause (player) {
+      // console.log('player pause!', player)
+    },
+    // ...player event
+
+    // or listen state event
+    playerStateChanged (playerCurrentState) {
+      // console.log('player current update state', playerCurrentState)
+    },
+
+    // player is ready
+    playerReadied (player) {
+      console.log('the player is readied', player)
+      // you can use it to do something...
+      // player.[methods]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ video/src/components/video.vue
@@ -0,0 +1,16 @@
+<template>
+ <!-- <video src="https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm" controls="controls"> -->
+ <video src="https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm" width="320" height="240" controls="controls">
+Your browser does not support the video tag.
+</video>
+
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+<style>
+</style>
